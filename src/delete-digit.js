e76669c7ff57a5ddef66e083752cbf32d05dad8c
FarAK10 <botirovfarhod115@gmail.com>
2022-04-30 23:34:44 +0500

    feat:solve 12 tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+   let number = n.toString().split("").map(n => parseInt(n));
+  let arr = [];
+  for (let i = 0; i < number.length; i++){
+    let arr1 = n.toString().split("")
+    arr1.splice(i, 1);
+    arr.push(arr1);
+  }
+  arr = arr.map(n=>parseInt(n.join("")))
+  arr.sort((a, b) => a - b);
+  return arr[arr.length-1]
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(arr) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (Array.isArray(arr)) {
+     arr1 = arr.filter(n => typeof n === 'string').map(n => n.split(' ').join("").toUpperCase()).sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0));
+  let str = '';
+  for (let i = 0; i < arr1.length; i++){
+    str += arr1[i][0];
+  }
+  return str;
+  }
+  return false
 }
 
 module.exports = {
