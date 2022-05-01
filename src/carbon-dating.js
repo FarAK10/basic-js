d68ac0aba593def44e5a6a9d514659e25b5d0352
FarAK10 <botirovfarhod115@gmail.com>
2022-05-01 23:41:32 +0500

    cipher left

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
   // remove line with error and write your code here
+  if (sampleActivity <1|| !sampleActivity ||typeof sampleActivity !== "string" ||!sampleActivity ||sampleActivity > 15) {
+    return false;
+  }
+  else if (isNaN(sampleActivity)) {
+    return false
+  }
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+    
 }
 
 module.exports = {
